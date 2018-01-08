bf662dd88923e4f7aa3474727b37a2a8c6da692b
ruguoxiansheng <by_wwj@163.com>
2018-01-08 13:40:03 +0800

    理了一下流程。

--- src/main/java/com/my/controller/ThymeleafController.java
+++ src/main/java/com/my/controller/ThymeleafController.java
@@ -2,7 +2,9 @@ package com.my.controller;
 
 import java.io.IOException;
 
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 import org.apache.http.HttpResponse;
@@ -28,27 +30,35 @@ public class ThymeleafController {
 	private Environment env;
 
 	@RequestMapping(value = "/app1")
-	public String pageLogin(HttpServletRequest request) throws Exception {
-		
+	public String pageLogin(HttpServletRequest request, HttpServletResponse response) throws Exception {
+
 		HttpSession session = request.getSession();
 
 		// 如果localSeeionId不存在，就重定向到SSOServer的接口/sso/page/login
 		if (session == null || session.getId() == null) {
-			
-			// 去server验证是否登陆
+
+			// 去server验证是否登陆,没有登录的话，server会给浏览器一个登录界面，如果登录了，会返回一个token
 			redirectToServer(request, "localhost:8077", "/sso/page/login");
 
-			// 生成局部
+			// 拿着token去验证是否正确，调用server的接口是：/sso/auth/verify
+
+			// 验证成功之后生成局部httpSession
 			session = request.getSession(true);
 			String localSessionId = session.getId();
-			LocalSessions.addSession(localSessionId,session);
+			LocalSessions.addSession(localSessionId, session);
+
+			// 采用cookie的方式记录下两个sessionId
+			Cookie localSessionCookie = new Cookie("localSessionId", "cookievalue");
+			Cookie globalSessionCookie = new Cookie("globalSessionId", "cookievalue");
+			response.addCookie(globalSessionCookie);
+			response.addCookie(localSessionCookie);
 		}
 
 		return "/app1";
 
 	}
 
-	//"/sso/page/login"
+	// "/sso/page/login"
 	private void redirectToServer(HttpServletRequest request, String server, String address)
 			throws ClientProtocolException, IOException {
 		String verifyURL = "http://" + server + address;
