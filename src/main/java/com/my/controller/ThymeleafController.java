d71caf177fda0825b45ff2c01655deba08d8a207
ruguoxiansheng <by_wwj@163.com>
2018-01-09 08:42:37 +0800

    数据库配置。

--- src/main/java/com/my/controller/ThymeleafController.java
+++ src/main/java/com/my/controller/ThymeleafController.java
@@ -13,7 +13,6 @@ import org.apache.http.HttpResponse;
 import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
-import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.util.EntityUtils;
 import org.springframework.beans.factory.annotation.Autowired;
