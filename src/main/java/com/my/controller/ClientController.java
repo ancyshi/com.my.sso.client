888782dec859909dc64b4916c416a7075d87b697
ruguoxiansheng <by_wwj@163.com>
2018-01-09 14:48:05 +0800

    理流程，待测试。

--- src/main/java/com/my/controller/ClientController.java
+++ src/main/java/com/my/controller/ClientController.java
@@ -1,11 +1,15 @@
 package com.my.controller;
 
+import java.io.IOException;
+
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import org.apache.http.HttpResponse;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
-import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.util.EntityUtils;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,7 +19,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.alibaba.fastjson.JSONObject;
-import com.fasterxml.jackson.databind.ObjectMapper;
+import com.my.util.LocalSessions;
 
 @Controller
 @RequestMapping(value = "/client")
@@ -24,7 +28,7 @@ public class ClientController {
 	private Environment env;
 
 	@RequestMapping(value = "/auth/check")
-	public String authCheck(HttpServletRequest request){
+	public String authCheck(HttpServletRequest request, HttpServletResponse response) throws IOException {
 		// 接受来自认证中心的token
 		String token = request.getParameter("token");
 		if (token == null) {
@@ -33,8 +37,25 @@ public class ClientController {
 
 		// 去server端验证token的真伪
 		checkToken(token, request);
-		
-		return (String)request.getAttribute("returnURL");
+
+		// 如有效简历本地会话
+		HttpSession session = request.getSession(true);
+		String localSessionId = session.getId();
+		LocalSessions.addSession(localSessionId, session);
+
+		// 采用cookie的方式记录下两个sessionId
+		Cookie localSessionCookie = new Cookie("app1SessionId", "cookievalue");
+		localSessionCookie.setPath("/");
+		localSessionCookie.setSecure(true);
+		Cookie globalSessionCookie = new Cookie("globalSessionId", "cookievalue");
+		globalSessionCookie.setPath("/");
+		globalSessionCookie.setSecure(true);
+		response.addCookie(globalSessionCookie);
+		response.addCookie(localSessionCookie);
+
+		response.sendRedirect("http://localhost:8077" + request.getAttribute("returnURL"));
+
+		return (String) request.getAttribute("returnURL");
 	}
 
 	@RequestMapping(value = "/auth/logout")
@@ -59,7 +80,7 @@ public class ClientController {
 			int statusCode = httpResponse.getStatusLine().getStatusCode();
 			if (statusCode == HttpStatus.OK.value()) {
 				String result = EntityUtils.toString(httpResponse.getEntity(), "utf-8");
-				
+
 				return "redirect:http://" + result;
 			}
 		} catch (Exception e) {
--- src/main/java/com/my/controller/ThymeleafController.java
+++ src/main/java/com/my/controller/ThymeleafController.java
@@ -1,13 +1,9 @@
 package com.my.controller;
 
 import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
 
-import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 
 import org.apache.http.HttpResponse;
 import org.apache.http.client.ClientProtocolException;
@@ -23,7 +19,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
-import com.my.util.LocalSessions;
 import com.my.util.ToolsUtil;
 
 @Controller
@@ -36,44 +31,47 @@ public class ThymeleafController {
 	@RequestMapping(value = "/app1")
 	public String pageLogin(HttpServletRequest request, HttpServletResponse response) throws Exception {
 
-		HttpSession session = request.getSession();
-		
-		
-		String app1SessionId = ToolsUtil.getCookieValueByName(request,"app1SessionId");
-		
-		String returnURL = "/login";
+		// HttpSession session = request.getSession();
+
+		String app1SessionId = ToolsUtil.getCookieValueByName(request, "app1SessionId");
+
+		String returnURL = (String) request.getAttribute("returnURL");
 
 		// 如果localSeeionId不存在，就重定向到SSOServer的接口/sso/page/login
 		if (app1SessionId == null) {
-
-			// 去server验证是否登陆,没有登录的话，server会给浏览器一个登录界面，如果登录了，会返回一个token
-			JSONObject resultObj = redirectToServer(request, "localhost:8077", "/sso/page/login");
-
-			
-			if (resultObj.get("tokenInfo") == null) {
-				return resultObj.getString("returnURL");
-			}
-			// 拿着token去验证是否正确，调用server的接口是：/sso/auth/verify
-			JSONObject tokenVerify = redirectToServer(request,"localhost:8077", "/sso/auth/verify");
-			
-			if (!tokenVerify.getBooleanValue("tokenVerify")) {
-				return tokenVerify.getString("returnURL");
-			}
-			
-			// 验证成功之后生成局部httpSession
-			session = request.getSession(true);
-			String localSessionId = session.getId();
-			LocalSessions.addSession(localSessionId, session);
-
-			// 采用cookie的方式记录下两个sessionId
-			Cookie localSessionCookie = new Cookie("app1SessionId", "cookievalue");
-			localSessionCookie.setPath("/");
-			localSessionCookie.setSecure(true);
-			Cookie globalSessionCookie = new Cookie("globalSessionId", "cookievalue");
-			globalSessionCookie.setPath("/");
-			globalSessionCookie.setSecure(true);
-			response.addCookie(globalSessionCookie);
-			response.addCookie(localSessionCookie);
+			// 重定向到认证中心
+			response.sendRedirect("http://localhost:8077/sso/page/login?returnURL=app1");
+			// // 去server验证是否登陆,没有登录的话，server会给浏览器一个登录界面，如果登录了，会返回一个token
+			// JSONObject resultObj = redirectToServer(request,
+			// "localhost:8077", "/sso/page/login");
+			//
+			// if (resultObj.get("tokenInfo") == null) {
+			// return resultObj.getString("returnURL");
+			// }
+			// // 拿着token去验证是否正确，调用server的接口是：/sso/auth/verify
+			// JSONObject tokenVerify = redirectToServer(request,
+			// "localhost:8077", "/sso/auth/verify");
+			//
+			// if (!tokenVerify.getBooleanValue("tokenVerify")) {
+			// return tokenVerify.getString("returnURL");
+			// }
+			//
+			// // 验证成功之后生成局部httpSession
+			// session = request.getSession(true);
+			// String localSessionId = session.getId();
+			// LocalSessions.addSession(localSessionId, session);
+			//
+			// // 采用cookie的方式记录下两个sessionId
+			// Cookie localSessionCookie = new Cookie("app1SessionId",
+			// "cookievalue");
+			// localSessionCookie.setPath("/");
+			// localSessionCookie.setSecure(true);
+			// Cookie globalSessionCookie = new Cookie("globalSessionId",
+			// "cookievalue");
+			// globalSessionCookie.setPath("/");
+			// globalSessionCookie.setSecure(true);
+			// response.addCookie(globalSessionCookie);
+			// response.addCookie(localSessionCookie);
 		}
 
 		return returnURL;
@@ -83,23 +81,23 @@ public class ThymeleafController {
 	// "/sso/page/login"
 	private JSONObject redirectToServer(HttpServletRequest request, String server, String address)
 			throws ClientProtocolException, IOException {
-		
+
 		JSONObject resultObj = new JSONObject();
 		String verifyURL = "http://" + server + address;
 
 		HttpClient httpClient = HttpClients.custom().build();
-		
-		HttpGet httpGet = new HttpGet(verifyURL + "?glocalSession=" + request.getSession());
+
+		HttpGet httpGet = new HttpGet(verifyURL);
 		try {
 			HttpResponse httpResponse = httpClient.execute(httpGet);
 			int statusCode = httpResponse.getStatusLine().getStatusCode();
 			if (statusCode == HttpStatus.OK.value()) {
-				
+
 				String result = EntityUtils.toString(httpResponse.getEntity(), "utf-8");
-				
-				 resultObj = JSON.parseObject(result);
+
+				resultObj = JSON.parseObject(result);
 				return resultObj;
-			
+
 			}
 		} catch (Exception e) {
 			// 返回登录页面
@@ -110,8 +108,5 @@ public class ThymeleafController {
 		return resultObj;
 
 	}
-	
-
-
 
 }
